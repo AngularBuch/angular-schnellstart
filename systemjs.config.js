e8c99257aa99369193355e39e301fe75e9de9d58
Gregor Woiwode <gregor.woiwode@gmail.com>
2017-11-09 18:14:21 +0100

    update to Angular 5 :rocket: Tested on: - Chrome - Internet Explorer - Firefox

--- systemjs.config.js
+++ systemjs.config.js
@@ -1,4 +1,4 @@
-var version = '4.0.0';
+const version = '@5.0.0';
 
 System.config({
   transpiler: 'ts',
@@ -14,14 +14,14 @@ System.config({
   },
   paths: { 'npm:': 'https://unpkg.com/' },
   map: {
-    '@angular/common': 'npm:@angular/common@' + version + '/bundles/common.umd.js',
-    '@angular/compiler': 'npm:@angular/compiler@' + version + '/bundles/compiler.umd.js',
-    '@angular/core': 'npm:@angular/core@' + version + '/bundles/core.umd.js',
-    '@angular/forms': 'npm:@angular/forms@' + version + '/bundles/forms.umd.js',
-    '@angular/http': 'npm:@angular/http@' + version + '/bundles/http.umd.js',
-    '@angular/platform-browser': 'npm:@angular/platform-browser@' + version + '/bundles/platform-browser.umd.js',
-    '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic@' + version + '/bundles/platform-browser-dynamic.umd.js',
-    '@angular/router': 'npm:@angular/router@' + version + '/bundles/router.umd.js',
+    '@angular/common': 'npm:@angular/common' + version + '/bundles/common.umd.js',
+    '@angular/compiler': 'npm:@angular/compiler' + version + '/bundles/compiler.umd.js',
+    '@angular/core': 'npm:@angular/core' + version + '/bundles/core.umd.js',
+    '@angular/forms': 'npm:@angular/forms' + version + '/bundles/forms.umd.js',
+    '@angular/http': 'npm:@angular/http' + version + '/bundles/http.umd.js',
+    '@angular/platform-browser': 'npm:@angular/platform-browser' + version + '/bundles/platform-browser.umd.js',
+    '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic' + version + '/bundles/platform-browser-dynamic.umd.js',
+    '@angular/router': 'npm:@angular/router' + version + '/bundles/router.umd.js',
     'rxjs': 'npm:rxjs',
     'ts': 'npm:plugin-typescript/lib/plugin.js',
     'typescript': 'npm:typescript/lib/typescript.js'
